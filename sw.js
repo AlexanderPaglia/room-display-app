80356ae913774802228bfb1cb82e33485810ff03
Alexander Paglia <46406901+AlexanderPaglia@users.noreply.github.com>
2025-08-05 13:18:20 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// sw.js - A minimal service worker to allow the app to be installable.
+self.addEventListener('fetch', (event) => {
+  // This listener is sufficient to meet the PWA installability criteria.
+});
